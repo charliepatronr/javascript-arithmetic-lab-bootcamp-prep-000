34edc743317969c5ad02d7a40a6bb7d614e792eb
Juan Carlos Patron <charliepatronr@gmail.com>
2019-12-30 04:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+
+
+function add(a, b){
+  return  a + b;
+}
+
+function subtract(a, b){
+  return a - b; 
+}
+
+function multiply (a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b ;
+}
+
+function inc(n){
+  return n++;
+}
+
+function dec(n){
+  return n--;
+}
+
+function makeInt(n){
+  return pars
+}
+
+//   7) makeInt(n) parses n as an integer and returns the parsed integer:
+//      ReferenceError: makeInt is not defined
+//       at Context.<anonymous> (test/index-test.js:34:5)
+
+//   8) makeInt(n) assumes base 10:
+//      ReferenceError: makeInt is not defined
+//       at Context.<anonymous> (test/index-test.js:38:5)
